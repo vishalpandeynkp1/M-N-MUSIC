7decab5896dbf470f08b16e7a8f92a18c1a526a3
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-27 12:26:32 +0530

    Update call.py

--- BADMUSIC/core/call.py
+++ BADMUSIC/core/call.py
@@ -1,6 +1,12 @@
 # Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
 # Owner https://t.me/ll_BAD_MUNDA_ll
 
+
+
+
+
+
+
 import asyncio
 import os
 from datetime import datetime, timedelta
