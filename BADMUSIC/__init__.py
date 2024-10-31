9a0477d181d6b5fc590a3e476ad632020674c540
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-31 23:38:45 +0530

    Update __init__.py

--- BADMUSIC/__init__.py
+++ BADMUSIC/__init__.py
@@ -1,8 +1,6 @@
 import json
 import os
 import config
-import pytz
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from BADMUSIC.core.bot import BADBOT
 from BADMUSIC.core.dir import dirr
@@ -12,12 +10,7 @@ from BADMUSIC.core.youtube import badmunda
 from BADMUSIC.misc import dbb, heroku, sudo
 
 from .logging import LOGGER
-from .loggeer import SUKH2
 
-
-#time zone
-TIME_ZONE = pytz.timezone(config.TIME_ZONE)
-scheduler = AsyncIOScheduler(timezone=TIME_ZONE)
 
 dirr()
 
