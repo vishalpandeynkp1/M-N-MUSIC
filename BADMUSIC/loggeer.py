671606a031c9117a8aa4b080ebf746e10104495b
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-31 22:56:20 +0530

    Update loggeer.py

--- BADMUSIC/loggeer.py
+++ BADMUSIC/loggeer.py
@@ -1,16 +1,23 @@
-
 import logging
-from logging.handlers import RotatingFileHandler
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+import pytz
+import time
 from pymongo import MongoClient
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from Abg import patch
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram import Client
 from pyrogram.enums import ParseMode
-
 import config
-import time
+import uvloop
 
+uvloop.install()
 
-# welcome..
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 boot = time.time()
