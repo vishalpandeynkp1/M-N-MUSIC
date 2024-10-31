2319b6ebaee6ba98ed28277a872254f7623e2fb7
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-31 22:58:24 +0530

    Update loggeer.py

--- BADMUSIC/loggeer.py
+++ BADMUSIC/loggeer.py
@@ -26,3 +26,24 @@ db = mongodb.Badmusic
 mongo = MongoClient(config.MONGO_DB_URI)
 OWNER = config.OWNER_ID
 
+class app(Client):
+    def __init__(self):
+        super().__init__(
+            name="shizuchat",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            lang_code="en",
+            bot_token=config.BOT_TOKEN,
+            in_memory=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+
+    async def stop(self):
+        await super().stop()
