f7d28581a2d3a87ecd9fa28ae6f690e1eefd936c
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-31 23:30:11 +0530

    Update loggeer.py

--- BADMUSIC/loggeer.py
+++ BADMUSIC/loggeer.py
@@ -28,28 +28,6 @@ db = mongodb.Badmusic
 mongo = MongoClient(config.MONGO_DB_URI)
 OWNER = config.OWNER_ID
 
-class app(Client):
-    def __init__(self):
-        super().__init__(
-            name="app",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            lang_code="en",
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.DEFAULT,
-        )
-
-    async def start(self):
-        await super().start()
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
-
-    async def stop(self):
-        await super().stop()
-
 
 def SUKH2(name: str) -> logging.Logger:
     return logging.getLogger(name)
