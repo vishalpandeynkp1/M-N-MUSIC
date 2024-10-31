6d0c92aed9bf679e33782460f1343cfa52fbeaed
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-10-31 23:30:56 +0530

    Update loggeer.py

--- BADMUSIC/loggeer.py
+++ BADMUSIC/loggeer.py
@@ -12,14 +12,16 @@ from pyrogram.enums import ParseMode
 import config
 import uvloop
 
-uvloop.install()
-
 logging.basicConfig(
+    level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=5000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
 )
+
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 SUKH2 = logging.getLogger(__name__)
 boot = time.time()
