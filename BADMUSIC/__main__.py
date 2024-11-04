aed91a2490f8d42c6322f9a29e355e524c7b1505
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-04 17:28:28 +0530

    Update __main__.py

--- BADMUSIC/__main__.py
+++ BADMUSIC/__main__.py
@@ -1,8 +1,4 @@
-# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
-# Owner https://t.me/ll_BAD_MUNDA_ll
 
-import asyncio
-import importlib
 
 from pyrogram import idle
 
